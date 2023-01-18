662e8811d912ece6d39291023ffa34ebee8f887f
Grey Altr <grey.sifr@gmail.com>
2023-01-18 15:10:58 -0800

    data fetched into App.js from Countries.js

--- src/services/Countries.js
+++ src/services/Countries.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import { checkError, client } from './client.js';
 
-export default function Countries() {
-  return <div>Countries</div>;
+export async function getCountries() {
+  const resp = await client.from('countries').select('*');
+  console.log('resp', resp.data);
+  return checkError(resp);
 }
+
+getCountries();
