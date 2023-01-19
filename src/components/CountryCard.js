057508918049e4543658e02eb11f173d4c811e60
Grey Altr <grey.sifr@gmail.com>
2023-01-18 16:48:57 -0800

    flags display on page

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function countryCard({ name }) {
+export default function countryCard({ name, iso2 }) {
   return (
     <div className="countryCard">
       <h3 className="name">{name}</h3>
-      {/* <img className="flag">{iso2}</img> */}
+      <img src={`https://flagcdn.com/120x90/${iso2.toLowerCase()}.png`} />
     </div>
   );
 }
